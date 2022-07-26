e34e51699a2860706c5dc5a867df14e0e099a8da
Ismaël Pcro <ismaelpecoraro@icloud.com>
2022-07-26 22:36:23 +0200

    First Push

---
+++ assets/header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Le TEC (Backup 1658754067383)</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/Bootstrap-DataTables.css">
+    <link rel="stylesheet" href="assets/css/Hero-Carousel-images.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/Navbar-Right-Links-Dark-icons.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+    <script src="assets/js/Bootstrap-DataTables.js"></script>
+    <script src="assets/js/Lightbox-Gallery.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark navbar-expand-md bg-dark py-3">
+        <div class="container-fluid"><a class="navbar-brand d-flex align-items-center" href="#"><span><img src="https://upload.wikimedia.org/wikipedia/commons/7/70/TEC_Wallonne_logo.png" width="120" height="40"></span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-5"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-5">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link active" href="index.php">Accueil</a></li>
+                    <li class="nav-item"><a class="nav-link" href="about.php"><span style="color: rgb(255, 255, 255);">Qui sommes-nous ?</span></a></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"></li>
+                    <li class="nav-item"><a class="nav-link" href="#"><span style="color: rgb(255, 255, 255);">Actualités</span></a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.php"><span style="color: rgb(255, 255, 255);">Contact</span></a></li>
+                </ul><a class="btn btn-primary ms-md-2" role="button" href="#" style="background: rgb(236,30,36);">Téléchargements</a>
+            </div>
+        </div>
+    </nav>
+    <div class="carousel slide" data-bs-ride="carousel" id="carousel-1" style="height: 600px;">
+        <div class="carousel-inner h-100">
+            <div class="carousel-item active h-100"><img class="w-100 d-block position-absolute h-100 fit-cover" src="assets/img/Omsi_2022-01-11_19-23-29.png" alt="Slide Image" style="z-index: -1;">
+                <div class="container d-flex flex-column justify-content-center h-100">
+                    <div class="row">
+                        <div class="col-md-6 col-xl-4 offset-md-2">
+                            <div style="max-width: 350px;">
+                                <h1 class="text-uppercase fw-bold"><span style="color: rgb(255, 255, 255);">Réseau TEC</span></h1>
+                                <p class="my-3"><span style="color: rgb(255, 255, 255);">Votre carte 100% Liégeoise basée sur les lignes réelles du TEC Liège-Verviers</span></p><a class="btn btn-primary btn-lg me-2" role="button" href="#" style="background: rgb(236,30,36);">Téléchargements</a><a class="btn btn-primary btn-lg" role="button" href="#" style="color: rgb(255,255,255);background: rgb(252,221,36);">Informations</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div><a class="carousel-control-prev" href="#carousel-1" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon"></span><span class="visually-hidden">Previous</span></a><a class="carousel-control-next" href="#carousel-1" role="button" data-bs-slide="next"><span class="carousel-control-next-icon"></span><span class="visually-hidden">Next</span></a></div>
+        <ol class="carousel-indicators">
+            <li data-bs-target="#carousel-1" data-bs-slide-to="0" class="active"></li>
+        </ol>
+    </div>
+    <p></p>
