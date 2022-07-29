5ff17dbb27fe458b3d3f2977fa0cc57e92d013c4
Ismaël Pcro <ismaelpecoraro@icloud.com>
2022-07-29 17:26:45 +0200

    Push

---
+++ V1/index.php
@@ -0,0 +1,55 @@
+<?php
+include('assets/header.php');
+?>
+
+    <title>Le TEC Virtuel | Accueil</title>
+    <div class="container-fluid">
+        <div class="alert alert-success alert-danger" role="alert"><span><strong>/!\ Maintenance /!\</strong> Notre site internet est en cours de développement. Merci de bien vouloir patienter...</span></div>
+    </div>
+    <p></p>
+    <div class="row">
+        <div class="col">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6 col-xl-9">
+                        <div class="card">
+                            <div class="card-header">
+                                <h5 class="mb-0"><i class="fas fa-bus"></i>&nbsp;Une carte 100% Belge dans un monde de passionnés</h5>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">Si très peu de personnes connaissent le réseau TEC en Belgique , d'autres seront ravis.</p>
+                                <p>En effet , notre map basée sur les lignes réelles est en cours de développement.</p>
+                                <p>Pour le moment, nous n'avons pas encore de date pour la sortie bêta de la carte mais nous sommes déja ravis de pouvoir mettre en place notre site internet qui vous permettra de télécharger notre carte dès qu'elle sera disponible.</p>
+                                <p>Nous vous souhaitons déja , la bienvenue sur notre site de la compagnie du TEC Virtuel</p>
+                            </div>
+                        </div>
+                        <section class="photo-gallery py-4 py-xl-5">
+                            <div class="container">
+                                <div class="row mb-5">
+                                    <div class="col-md-8 col-xl-6 text-center mx-auto">
+                                        <h2>Réseau TEC Virtuel</h2>
+                                        <p class="w-lg-50">Une map basée sur les lignes réelles du TEC Liège-Verviers.</p>
+                                    </div>
+                                </div>
+                                <div class="row gx-2 gy-2 row-cols-1 row-cols-md-2 row-cols-xl-3 photos" data-bss-baguettebox="">
+                                    <div class="col item"><a href="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"><img class="img-fluid" src="/assets/img/1.jpg"></a></div>
+                                    <div class="col item"><a href="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"><img class="img-fluid" src="/assets/img/2.jpg"></a></div>
+                                    <div class="col item"><a href="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"><img class="img-fluid" src="/assets/img/3.jpg"></a></div>
+                                    <!-- <div class="col item"><a href="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"><img class="img-fluid" src="assets/img/Omsi_2022-01-11_19-23-29.png"></a></div>
+                                    <div class="col item"><a href="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"><img class="img-fluid" src="assets/img/Omsi_2022-01-11_19-23-29.png"></a></div>
+                                    <div class="col item"><a href="https://cdn.bootstrapstudio.io/placeholders/1400x800.png"><img class="img-fluid" src="assets/img/Omsi_2022-01-11_19-23-29.png"></a></div> -->
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    <div class="col-md-6 col-xl-2"><iframe src="https://discordapp.com/widget?id=691619913973891095&theme=dark" width="450" height="450" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
+                        <p></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php
+include('assets/footer.php');
+?>
